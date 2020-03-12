ac0e154fa468ed407e3e7a8f48461d4d8e79528d
Kajol <kajolgupta@mirafra.com>
2020-03-12 12:59:56 +0530

    new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBb6XKMHsH6kzhOGGFP_As0aNKJsKE1TRg',
+      authDomain: 'ng-shopping-cart-8c90c.firebaseapp.com'
+    });
+  }
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Tasty Speghetti',
+      'A super-tasty Speghatti - just awesome!',
+      'https://images8.alphacoders.com/826/826205.jpg',
+      [
+        new Ingredient('Speghetti', 1),
+        new Ingredient('Besil', 20)
+      ]),
+    new Recipe('Big Bowl Pasta',
+      'What else you need to say?',
+      'https://i.ytimg.com/vi/63HiDpiuMFQ/maxresdefault.jpg',
+      [
+        new Ingredient('Pasta', 2),
+        new Ingredient('Tamotos', 1)
+      ])
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+}
